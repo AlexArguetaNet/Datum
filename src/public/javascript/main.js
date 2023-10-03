934fcb091934521ede8bb2754f8d2df550495646
Alex Argueta <alexarguetanet@gmail.com>
2023-10-03 17:53:01 -0400

    Fixed the pop up functionality and started working on the user home page UI

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -4,24 +4,20 @@ window.onload = () => {
     htmlNode = document.getElementsByTagName("html")[0];
     htmlNode.id = "html";
 
-    // index.ejs
-    signUpBlock = document.getElementById("sign-up-block");
-    signUpBlock.style.display = "none";
+    popUpElem = document.getElementsByClassName("popup")[0];
+    popUpElem.style.display = "none";
 } 
 
-// index.ejs
-const showSignUp = (event) => {
 
-    if (signUpBlock.style.display == "none") {
-        signUpBlock.style.display = "flex";
-        signUpBlock.style.backgroundColor = "rgba(0, 0, 0, .5)";
-        // rgba(81, 118, 100, .5)
+const showPopUp = (event) => {
+
+    if (popUpElem.style.display == "none") {
+        popUpElem.style.display = "block";
+        popUpElem.style.backgroundColor = "rgba(0, 0, 0, .5)";
     } else {
-        // Ensure that the sign up form only closes when
-        // clicked outside the form
-        if (event.target.id == "sign-up-block") {
-            event.target.style.display = "none";
-            signUpBlock.style.backgroundColor = "white"
+        if (event.target.classList.contains('popup')) {
+            popUpElem.style.display = "none";
+            popUpElem.style.backgroundColor = "white"
         }
     }
 
