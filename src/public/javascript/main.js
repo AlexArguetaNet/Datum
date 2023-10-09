be588edffc7ea8ab526806ec6db21de2d88893a3
Alex Argueta <alexarguetanet@gmail.com>
2023-10-09 15:54:54 -0400

    Improved the popup window functionality

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -1,19 +1,33 @@
 
 window.onload = () => {
     
-    popUpElem = document.getElementsByClassName("popup")[0];
-    popUpElem.style.display = "none";
+    try {
+        popUpElements = document.getElementsByClassName("popup");
+    
+        for (let i = 0; i < popUpElements.length; i++) {
+            popUpElements[i].style.display = "none";
+        }
+    } catch(e) {
+        console.log('No popups on this page');
+    }
+
 } 
 
 
 const showPopUp = (event) => {
 
+    const popUpElem = document.querySelectorAll('.popup.' + event.target.classList[1])[0];
+
     if (popUpElem.style.display == "none") {
         popUpElem.style.display = "block";
-    } else {
-        if (event.target.classList.contains('popup')) {
-            popUpElem.style.display = "none";
-        }
     }
 
+}
+
+const closePopUp = (event) => {
+
+    const popUpElem = document.querySelectorAll('.popup.' + event.target.classList[1])[0];
+    if (event.target.classList.contains('popup')) {
+        popUpElem.style.display = "none";
+    }
 }
