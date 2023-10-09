dc1b27c542123a1b7d32e3a3b73bbcff17f6099a
Alex Argueta <alexarguetanet@gmail.com>
2023-10-09 01:33:29 -0400

    Edited the text input for design for the sign up form

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -10,11 +10,9 @@ const showPopUp = (event) => {
 
     if (popUpElem.style.display == "none") {
         popUpElem.style.display = "block";
-        popUpElem.style.backgroundColor = "rgba(0, 0, 0, .5)";
     } else {
         if (event.target.classList.contains('popup')) {
             popUpElem.style.display = "none";
-            popUpElem.style.backgroundColor = "white"
         }
     }
 
