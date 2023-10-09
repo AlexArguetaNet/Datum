dac3a555089c3505e5f9548e914e62149b9ac0ec
Alex Argueta <alexarguetanet@gmail.com>
2023-10-09 18:21:23 -0400

    Made changes to the user home page

--- src/public/javascript/main.js
+++ src/public/javascript/main.js
@@ -3,11 +3,12 @@ window.onload = () => {
     
     // Check for popup elements in the html
     try {
+
         popUpElements = document.getElementsByClassName("popup");
-    
         for (let i = 0; i < popUpElements.length; i++) {
             popUpElements[i].style.display = "none";
         }
+        
     } catch(e) {
         console.log('No popups on this page');
     }
@@ -16,16 +17,13 @@ window.onload = () => {
 
 
 const showPopUp = (event) => {
-
     const popUpElem = document.querySelectorAll('.popup.' + event.target.classList[1])[0];
     if (popUpElem.style.display == "none") {
         popUpElem.style.display = "block";
     }
-
 }
 
 const closePopUp = (event) => {
-
     const popUpElem = document.querySelectorAll('.popup.' + event.target.classList[1])[0];
     if (event.target.classList.contains('popup')) {
         popUpElem.style.display = "none";
